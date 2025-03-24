b5786358d6af39cb2075a478f2e35e18be495f86
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-03-24 18:23:11 +0530

    base url change

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,3 +1,3 @@
-export const REACT_APP_BASE_URL = "https://ariprodesigns.co.uk/development/gabapentinshop/wp-content/themes/gabapentinshop/API"
+export const REACT_APP_BASE_URL = "https://webservices.gabapentinshop.com/wp-content/themes/gabapentinshop/API"
 export const REACT_APP_MAIL_URL = "https://gabapentinshop.com/api"
 // export const REACT_APP_MAIL_URL = "http://localhost:5000/api"

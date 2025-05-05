fc592df70f9c5de3692ec8ea989e5840487a7877
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-05-05 18:41:17 +0530

    build details

--- src/components/Product-details/ProductDetails.js
+++ src/components/Product-details/ProductDetails.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react'
 import Single_product from './SingleProduct'
 import usePageMeta from '../Seo/Seo'
-import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductDetails } from '../../store/thunks/productThunk'
 import Preloader from '../Preloader/Preloader'
 
 
 const ProductDetails = () => {
-  usePageMeta({
-    title: 'Product Details',
-    description: 'Gabapentinshop Product Details',
-    keywords: 'Gabapentinshop Product Details',
-  });
+
   const dispatch = useDispatch()
   const { productDetails } = useSelector(state => state.products)
+
+  usePageMeta({
+    title: productDetails?.meta_title,
+    description: productDetails?.meta_description,
+    keywords: productDetails?.meta_keyword,
+  });
   // const { product_id } = useParams()
   const productId = localStorage.getItem("prodId")
   useEffect(() => {
